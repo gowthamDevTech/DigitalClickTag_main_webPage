92c35f081b2f094394dd7896b355c66375450b63
gowthamDevTech <gowthamraajmg@gmail.com>
2025-07-08 17:06:33 +0530

    deploy errors

--- src/app/home2/page.tsx
+++ src/app/home2/page.tsx
@@ -1,16 +1,16 @@
 'use client'
-import Footer from "@/components/home_4/commonComponents/Footer";
-import Navbar from "@/components/home_4/commonComponents/Navbar";
-import Hero from "@/components/home/Hero";
-import ServicesSection from "@/components/home/ServicesSection";
-import WhyChooseUs from "@/components/home/WhyChooseUs";
-import PortfolioPreview from "@/components/home/PortfolioPreview";
-import Testimonials from "@/components/home/Testimonials";
-import ClientsSection from "@/components/home/ClientsSection";
-import PricingSection from "@/components/home/PricingSection";
-import BlogPreview from "@/components/home/BlogPreview";
-import CTASection from "@/components/home/CTASection";
-import ContactBanner from "@/components/home/ContactBanner";
+// import Footer from "@/components/home_4/commonComponents/Footer";
+// import Navbar from "@/components/home_4/commonComponents/Navbar";
+// import Hero from "@/components/home/Hero";
+// import ServicesSection from "@/components/home/ServicesSection";
+// import WhyChooseUs from "@/components/home/WhyChooseUs";
+// import PortfolioPreview from "@/components/home/PortfolioPreview";
+// import Testimonials from "@/components/home/Testimonials";
+// import ClientsSection from "@/components/home/ClientsSection";
+// import PricingSection from "@/components/home/PricingSection";
+// import BlogPreview from "@/components/home/BlogPreview";
+// import CTASection from "@/components/home/CTASection";
+// import ContactBanner from "@/components/home/ContactBanner";
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
@@ -22,7 +22,7 @@ export default function Home() {
   }, [router]);
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
       <main>
         <Hero />
         <ServicesSection />
@@ -30,12 +30,13 @@ export default function Home() {
         <PortfolioPreview />
         <Testimonials />
         <ClientsSection />
-        {/* <PricingSection /> */}
         <BlogPreview />
         <CTASection />
         <ContactBanner />
       </main>
-      <Footer />
+      <Footer /> */}
+        {/* <PricingSection /> */}
+
     </>
   );
 }
